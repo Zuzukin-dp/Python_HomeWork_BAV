ccd68fb910275f1aab0e633e6fef6e009e2f21b4
Zuzukin-dp <zuzukinz@gmail.com>
2021-01-12 09:35:31 +0200

    ДЗ №12

--- HomeWorks/BAV_HW_12.py
+++ HomeWorks/BAV_HW_12.py
@@ -1,3 +1,11 @@
+import re
+import random
+import json
+import csv
+import requests
+from datetime import datetime
+
+
 # Все пункты сделать как отдельные функции(можно создавать дополнительные вспомагательные функции)
 #
 # 1. Написать функцию, которая принимает в виде параметра целое число - количество цитат (см. урок 12).
@@ -6,15 +14,9 @@
 # Заголовки файла:
 # Author, Quote, URL. Если автор не указан, цитату не брать.
 # Перед сохранением в csv, записи отсортировать по автору (в алфавитном порядке).
-import re
-from datetime import datetime
-import json
-import random
-import csv
-import requests
 
 
-def get_quotes_by_api():
+def get_quote_by_api():
     params = {"method": "getQuote", "format": "json", "key": random.randint(0, 9999), "lang": "ru"}
     r = requests.get("http://api.forismatic.com/api/1.0/", params=params)
     quote = r.json()
@@ -24,7 +26,7 @@ def get_quotes_by_api():
                    "URL": quote["quoteLink"]}
         return my_dict
     else:
-        return get_quotes_by_api()
+        return get_quote_by_api()
 
 
 def key_sort_by_last_name(obj_dict):
@@ -41,7 +43,7 @@ def write_csv_file(list_dct, filename="pars_quotes.csv"):
 
 
 def get_list_quotes_by_api(len_list=10):
-    list_quotes = [get_quotes_by_api() for _ in range(len_list)]
+    list_quotes = [get_quote_by_api() for _ in range(len_list)]
     list_quotes = sorted(list_quotes, key=key_sort_by_last_name)
     return write_csv_file(list_quotes)
 
@@ -53,16 +55,17 @@ get_list_quotes_by_api()
 # 2.1) написать функцию, которая считывает данные из этого файла,
 # возвращая СПИСОК тех строк в которых есть полная дата, писатель и указание на его день рождения или смерти.
 # Например: 26th February 1802 - Victor Hugo's birthday - author of Les Misérables.
-# import re
-# from datetime import datetime
-# import json
+
+
+def key_reg_exp_author():
+    return re.compile(r"^\d{1,2}\D{2}\s\D+\s\d{4}(.+[dD]eath|.+[bB]irthday)(.+[aA]uthor|.+[cC]reator)")
+
 
 def generate_list_from_txt_file(filename="authors.txt"):
-    with open("authors.txt", "r") as f_txt:
-        s_name = [s_name.strip() for s_name in f_txt]
-        reg_exp_fdate = re.compile(r"^\d{1,2}\D{2}\s\D+\s\d{4}(.+[dD]eath|.+[bB]irthday)(.+[aA]uthor|.+[cC]reator)")
-        s_name = list(filter(reg_exp_fdate.match, s_name))
-        return s_name
+    with open(filename, "r") as f_txt:
+        author_lst = [author_lst.strip() for author_lst in f_txt]
+        author_lst = list(filter(key_reg_exp_author().match, author_lst))
+        return author_lst
 
 
 # print(reading_txt_file())
@@ -72,6 +75,15 @@ def generate_list_from_txt_file(filename="authors.txt"):
 # где name это имя автора, а date - дата из строки в формате "dd/mm/yyyy" (d-день, m-месяц, y-год)
 # Например [{"name": "Charles Dickens", "date": "09/06/1870"}, ...,
 # {"name": "J. D. Salinger", "date": "01/01/1919"}]
+
+
+def key_reg_exp_date():
+    return r"^\d{1,2}\D{2}\s\D+\s\d{4}"
+
+
+def key_reg_exp_name():
+    return r"[^\d\s-]\D+\s[A-Z]\w+['][s]"
+
 
 def convert_date(old_date):
     ord_num = ["st", "nd", "rd", "th"]
@@ -84,11 +96,9 @@ def convert_date(old_date):
 
 def generate_dict_from_list(list_name=generate_list_from_txt_file()):
     my_list = []
-    reg_exp_date = r"^\d{1,2}\D{2}\s\D+\s\d{4}"
-    reg_exp_name = r"[^\d\s-]\D+\s[A-Z]\w+['][s]"
     for values in list_name:
-        new_date = "".join(re.findall(reg_exp_date, values))
-        new_name = "".join(re.findall(reg_exp_name, values))[:-2]
+        new_date = "".join(re.findall(key_reg_exp_date(), values))
+        new_name = "".join(re.findall(key_reg_exp_name(), values))[:-2]
         new_date = convert_date(new_date)
         my_dict = {"name": new_name,
                    "date": new_date}
